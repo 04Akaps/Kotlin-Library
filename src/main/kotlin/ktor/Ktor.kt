ef02fba0315f432a191cca8929da5b3ead41a9bd
yuhojin <sdl182975@gmail.com>
2025-02-10 21:45:42 +0900

    Feat: ktorclient class

--- src/main/kotlin/ktor/Ktor.kt
+++ src/main/kotlin/ktor/Ktor.kt
@@ -1,12 +1,16 @@
 package ktor
 
 import io.ktor.client.HttpClient
+import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.util.*
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
 
 @Configuration
 class HttpClientConfig {
@@ -21,6 +25,41 @@ class HttpClientConfig {
             install(DefaultRequest) {
                 headers.appendIfNameAbsent(HttpHeaders.ContentType, ContentType.Application.Json.toString())
             }
+        }
+    }
+}
+
+
+@Component
+class KtorClient(private val http: HttpClient) {
+
+    suspend fun GET(url: String, headers: Map<String, String> = emptyMap()): HttpResponse {
+        return request(HttpMethod.Get, url, headers)
+    }
+
+    suspend fun POST(url: String, body: Any, headers: Map<String, String> = emptyMap()): HttpResponse {
+        return request(HttpMethod.Post, url, headers, body)
+    }
+
+    suspend fun PUT(url: String, body: Any, headers: Map<String, String> = emptyMap()): HttpResponse {
+        return request(HttpMethod.Put, url, headers, body)
+    }
+
+    suspend fun DELETE(url: String, headers: Map<String, String> = emptyMap()): HttpResponse {
+        return request(HttpMethod.Delete, url, headers)
+    }
+
+    private suspend fun  request(
+        method: HttpMethod,
+        url: String,
+        headers: Map<String, String>,
+        body: Any? = null
+    ): HttpResponse {
+        return http.request(url) {
+            this.method = method
+            headers.forEach { (key, value) -> header(key, value) }
+            contentType(ContentType.Application.Json)
+            body?.let { setBody(it) }
         }
     }
 }
