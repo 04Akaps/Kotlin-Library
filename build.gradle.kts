3fc11ab93f6ce17896232f9f04003327179202e9
jukas <jukas@wemade.com>
2025-02-14 14:07:18 +0900

    Feat: update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,41 +1,47 @@
 plugins {
-    id("org.springframework.boot") version "3.0.0"
-    id("io.spring.dependency-management") version "1.0.15.RELEASE"
-    kotlin("plugin.spring") version "1.8.0"
+    kotlin("jvm") version "1.8.0"
     kotlin("plugin.serialization") version "1.8.0"
-    kotlin("jvm") version "2.0.21"
+    id("maven-publish")
 }
 
 group = "org.library"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-
+    implementation(kotlin("stdlib"))
     implementation("io.ktor:ktor-client-core:2.2.3") // 최신 2.x 버전
     implementation("io.ktor:ktor-client-cio:2.2.3")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-
-    implementation("org.springframework.boot:spring-boot-starter")
-
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework:spring-tx") // 트랜잭션 관리만 사용할 경우
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-//    implementation("org.springframework.boot:spring-boot-starter-data-jpa") // JPA를 사용할 경우
-
-    implementation("org.springframework.boot:spring-boot-starter-cache")
-    implementation("org.springframework:spring-tx") // 트랜잭션 관리만 사용할 경우
+    testImplementation(kotlin("test"))
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
-
 
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(17)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            groupId = "org.library"
+            artifactId = "personal-library"
+            version = "1.0.0"
+        }
+    }
+    repositories {
+        maven {
+            url = uri("file://${project.buildDir}/maven-repo")
+        }
+    }
 }
