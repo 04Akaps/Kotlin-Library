52c49d02f9c8a888a749a7ba75b5c7b1649d53c0
jukas <jukas@wemade.com>
2025-02-14 14:11:11 +0900

    Feat: update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
-    kotlin("jvm") version "1.8.0"
+    id("org.springframework.boot") version "3.0.0"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    kotlin("plugin.spring") version "1.8.0"
     kotlin("plugin.serialization") version "1.8.0"
+    kotlin("jvm") version "1.8.0"
     id("maven-publish")
 }
 
@@ -13,10 +16,12 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("io.ktor:ktor-client-core:2.2.3") // 최신 2.x 버전
+    implementation("io.ktor:ktor-client-core:2.2.3")
     implementation("io.ktor:ktor-client-cio:2.2.3")
-    implementation("org.springframework:spring-tx") // 트랜잭션 관리만 사용할 경우
+    implementation("org.springframework:spring-tx")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 
     testImplementation(kotlin("test"))
     testImplementation("org.springframework.boot:spring-boot-starter-test")
